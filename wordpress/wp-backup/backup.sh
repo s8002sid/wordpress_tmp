110305e5c0dc376bdcc4705c94896608b2e5d661
root <root@localhost.localdomain>
2014-11-15 16:19:43 +0530

    Adding database backup corresponding to previous changes.

--- wordpress/wp-backup/backup.sh
+++ wordpress/wp-backup/backup.sh
@@ -1,1 +1,9 @@
+echo 'creating database backup...'
 mysqldump -u root -p wordpress > wordpress_bak.sql
+
+echo 'copying wordpress mysql file...'
+cp -r /var/lib/mysql/wordpress .
+
+echo 'creating tar and removing tmp folder...'
+tar -zcvf wordpress_sqldb.tgz ./wordpress
+rm -rf ./wordpress
