de8cbab1244579241ee1e48920a37ffaea163ca6
s8002sid <s8002sid@github.com>
2015-01-17 17:56:27 +0530

    Edited search box with product search

--- wordpress/wp-content/themes/skt-corp/searchform.php
+++ wordpress/wp-content/themes/skt-corp/searchform.php
@@ -11,4 +11,5 @@
 		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder', 'skt-corp' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 	</label>
 	<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'skt-corp' ); ?>">
+	<input type="hidden" name="post_type" value="product">
 </form>
